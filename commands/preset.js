75dbe998a35083249ef392ddcb894dca7fc2f360
Kianus <kian-helmink@hotmail.com>
2019-12-20 12:11:32 +0100

    Bot

---
+++ commands/preset.js
@@ -0,0 +1,10 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, arguments) => {
+    if (message.author.bot) return;
+
+};
+
+module.exports.help = {
+    name: "preset"
+};
